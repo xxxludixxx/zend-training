e8471763d3b9bf2120367fe8ac5d965ef608e054
Jakub Ludwa <jludwa@creatuity.pl>
2017-03-07 12:15:34 +0100

    2. Routing and controllers

---
+++ module/Album/src/Album/Controller/AlbumController.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jludwa
+ * Date: 07.03.17
+ * Time: 12:10
+ */
+namespace Album\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class AlbumController extends AbstractActionController
+{
+    public function indexAction()
+    {
+    }
+
+    public function addAction()
+    {
+    }
+
+    public function editAction()
+    {
+    }
+
+    public function deleteAction()
+    {
+    }
+}
